5328ec18c26e04f4b20f557666571e29b100f36b
Bhavesh suthar <bhaveshsutharkulariya123456@gmail.com>
2026-07-03 14:56:22 +0530

    Time: 5 ms (72.44%), Space: 64.4 MB (24.15%) - LeetHub

---
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    private static final int mod = 1_000_000_007;
+
+    public int kConcatenationMaxSum(int[] arr, int k) {
+        long total = 0;
+        long curr = 0, max1=0;
+
+        for(int x : arr){
+            curr = Math.max(0, curr+x);
+            max1=Math.max(max1 , curr);
+            total+=x;
+        }
+
+        if(k == 1) return (int)(max1%mod);
+
+        curr=0;
+        long max2=0;
+
+        for(int i=0;i<2*arr.length;i++){
+            curr=Math.max(0,curr+arr[i%arr.length]);
+            max2=Math.max(max2,curr);
+        }
+
+        if(total>0){
+            max2+=(long)(k-2)*total;
+        }
+
+        return (int)(max2%mod);
+    }
+}
