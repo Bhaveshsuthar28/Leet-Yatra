9691449bdeeaf213dddbebed5de68c9958850ec5
Bhavesh suthar <bhaveshsutharkulariya123456@gmail.com>
2026-08-26 19:16:27 +0530

    Time: 14 ms (28.47%), Space: 46.5 MB (22.28%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase().replaceAll("[^a-z0-9]" , "");
+
+        int l=0,r=str.length()-1;
+        while(l<r){
+            if(str.charAt(l) == str.charAt(r)){
+                l++;
+                r--;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
